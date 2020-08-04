1981f7fcb8fd03c11d66237bfad46f0836c94153
Maly <migsking@naver.com>
2020-08-04 19:04:26 +0900

    [CREATE] <TestAccountController.java>

---
+++ src/test/java/com/gym/dsm/fitness/controllers/TestAccountController.java
@@ -0,0 +1,19 @@
+package com.gym.dsm.fitness.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = AccountController.class)
+public class TestAccountController {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AccountService accountService;
+}
