0102eb9c6cb515efdd6fab1b0957b260e08ca42e
Maly <migsking@naver.com>
2020-08-05 22:36:43 +0900

    [ADD] <main/../controllers/AccountController.java>
    Create Account Controller

--- src/main/java/com/gym/dsm/fitness/controllers/AccountController.java
+++ src/main/java/com/gym/dsm/fitness/controllers/AccountController.java
@@ -1,4 +1,24 @@
-﻿package com.gym.dsm.fitness.controllers;
+package com.gym.dsm.fitness.controllers;
 
+import com.gym.dsm.fitness.payloads.request.CreateAccountRequest;
+import com.gym.dsm.fitness.payloads.response.CreateAccountResponse;
+import com.gym.dsm.fitness.services.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/account")
+@RequiredArgsConstructor
 public class AccountController {
+    private final AccountService accountService;
+
+    @PostMapping
+    public CreateAccountResponse createAccount(@RequestBody @Valid CreateAccountRequest createAccountRequest) {
+        return accountService.createAccount(createAccountRequest);
+    }
 }
