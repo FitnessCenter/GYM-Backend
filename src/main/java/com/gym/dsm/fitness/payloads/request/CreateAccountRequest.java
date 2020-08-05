0d5faa4c96f8fb259dc4926266d6ea5f627a2ae4
Maly <migsking@naver.com>
2020-08-05 20:24:01 +0900

    [CREATE] <main/payloads/request/CreateAccountRequest.java>

---
+++ src/main/java/com/gym/dsm/fitness/payloads/request/CreateAccountRequest.java
@@ -0,0 +1,26 @@
+package com.gym.dsm.fitness.payloads.request;
+
+import lombok.Getter;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+public class CreateAccountRequest {
+
+    @Max(4)
+    @NotEmpty
+    private String studentNumber;
+
+    @NotEmpty
+    private String studentName;
+
+    @NotEmpty
+    private String id;
+
+    @NotEmpty
+    private String password;
+    
+    private Boolean sex;
+}
