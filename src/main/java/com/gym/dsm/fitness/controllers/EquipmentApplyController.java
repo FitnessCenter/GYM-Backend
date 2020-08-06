468d4daae3334a4fe384cc86f3d8116d25d2c0a4
Eojin K1m <eojin.dev@gmail.com>
2020-08-06 21:14:58 +0900

    [CREATE]<main/../controllers/EquipmentApplyController.java>
    Add GET, POST handling method

---
+++ src/main/java/com/gym/dsm/fitness/controllers/EquipmentApplyController.java
@@ -0,0 +1,33 @@
+package com.gym.dsm.fitness.controllers;
+
+import com.gym.dsm.fitness.payloads.request.EquipmentApplyRequest;
+import com.gym.dsm.fitness.payloads.request.EquipmentApplyResponse;
+import com.gym.dsm.fitness.services.EquipmentApplyService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/equipment-applies")
+@RequiredArgsConstructor
+public class EquipmentApplyController {
+
+    private final EquipmentApplyService equipmentApplyService;
+
+
+    @GetMapping
+    public List<EquipmentApplyResponse> getEquipmentApplies(){
+        return equipmentApplyService.getEquipmentApplies();
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void createEquipmentApply(@RequestBody @Valid EquipmentApplyRequest request){
+        equipmentApplyService.createEquipmentApply(request);
+    }
+
+}
