fa673e646b4271bb00d86c54e31b9b5e2273ec04
Eojin K1m <eojin.dev@gmail.com>
2020-08-07 10:14:46 +0900

    [ADD]</main/../controllers/EquipmentApplyController.java> Delete method handler

--- src/main/java/com/gym/dsm/fitness/controllers/EquipmentApplyController.java
+++ src/main/java/com/gym/dsm/fitness/controllers/EquipmentApplyController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -28,6 +29,11 @@ public class EquipmentApplyController {
     @ResponseStatus(value = HttpStatus.CREATED)
     public void createEquipmentApply(@RequestBody @Valid EquipmentApplyRequest request){
         equipmentApplyService.createEquipmentApply(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEquipmentApply(@PathVariable Integer id){
+        equipmentApplyService.deleteEquipmentApply(id);
     }
 
 }
