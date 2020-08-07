ace52ece34478618aeccda51360e7f54519316ad
Eojin K1m <eojin.dev@gmail.com>
2020-08-07 10:14:08 +0900

    [ADD]</main/../services/EquipmentApplyService.java> deleteEquipmentApply func

--- src/main/java/com/gym/dsm/fitness/services/EquipmentApplyService.java
+++ src/main/java/com/gym/dsm/fitness/services/EquipmentApplyService.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface EquipmentApplyService {
     List<EquipmentApplyResponse> getEquipmentApplies();
     void createEquipmentApply(EquipmentApplyRequest request);
+    void deleteEquipmentApply(Integer id);
 }
