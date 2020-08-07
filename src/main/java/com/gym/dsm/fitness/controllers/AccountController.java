7a7b55924c33cff595cb6d3f31b146bd4a385796
Maly <migsking@naver.com>
2020-08-07 16:50:14 +0900

    [ADD] <main/../controllers/AccountController.java>
    Add the Status code 201

--- src/main/java/com/gym/dsm/fitness/controllers/AccountController.java
+++ src/main/java/com/gym/dsm/fitness/controllers/AccountController.java
@@ -4,10 +4,8 @@ import com.gym.dsm.fitness.payloads.requests.CreateAccountRequest;
 import com.gym.dsm.fitness.payloads.responses.CreateAccountResponse;
 import com.gym.dsm.fitness.services.AccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -17,6 +15,7 @@ import javax.validation.Valid;
 public class AccountController {
     private final AccountService accountService;
 
+    @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping
     public CreateAccountResponse createAccount(@RequestBody @Valid CreateAccountRequest createAccountRequest) {
         return accountService.createAccount(createAccountRequest);
