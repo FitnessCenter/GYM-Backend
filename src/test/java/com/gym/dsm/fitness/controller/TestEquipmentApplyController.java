c1c87bcdd6facfd7c0499ad83ab58495eda3c1e4
Eojin K1m <eojin.dev@gmail.com>
2020-08-07 09:57:36 +0900

    [ADD]</test/../controller/TestEquipmentApplyController.java> Tests
    Implement Create, Update, Delete method test

--- src/test/java/com/gym/dsm/fitness/controller/TestEquipmentApplyController.java
+++ src/test/java/com/gym/dsm/fitness/controller/TestEquipmentApplyController.java
@@ -1,15 +1,16 @@
 package com.gym.dsm.fitness.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gym.dsm.fitness.controllers.EquipmentApplyController;
-import com.gym.dsm.fitness.payloads.request.EquipmentApplyResponse;
+import com.gym.dsm.fitness.payloads.request.EquipmentApplyRequest;
+import com.gym.dsm.fitness.payloads.response.EquipmentApplyResponse;
 import com.gym.dsm.fitness.services.EquipmentApplyService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -29,6 +30,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class TestEquipmentApplyController {
 
     private final String url = "/equipment-applies";
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
 
     @Autowired
     MockMvc mvc;
@@ -49,7 +52,6 @@ public class TestEquipmentApplyController {
                 .purchaseLink("http://bitly.kr/ZUuEfi7UgVx")
                 .build()
         );
-
         when(equipmentApplyService.getEquipmentApplies()).thenReturn(response);
 
         mvc.perform(MockMvcRequestBuilders
@@ -60,23 +62,44 @@ public class TestEquipmentApplyController {
 
     @Test
     public void testCreateEquipmentApply() throws Exception{
-        ResultActions actions = mvc.perform(MockMvcRequestBuilders
-                .post(url))
+
+        EquipmentApplyRequest request = EquipmentApplyRequest
+                .builder()
+                .numberOfApply(12)
+                .price(3000)
+                .equipmentName("12kg 덤벨")
+                .purchaseLink("http://bitly.kr/ZUuEfi7UgVx")
+                .build();
+
+        mvc.perform(MockMvcRequestBuilders
+                .post(url)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(request)))
                 .andDo(print())
                 .andExpect(status().isCreated());
-
     }
 
     @Test void testUpdateEquipmentApply() throws Exception{
-        mvc.perform(MockMvcRequestBuilders
-                .put(url+"1"));
+        EquipmentApplyRequest request = EquipmentApplyRequest
+                .builder()
+                .numberOfApply(12)
+                .price(3000)
+                .equipmentName("12kg 덤벨")
+                .purchaseLink("http://bitly.kr/ZUuEfi7UgVx")
+                .build();
 
+        mvc.perform(MockMvcRequestBuilders
+                .put(url+"/1")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
     @Test
     public void testDeleteEquipmentApply() throws Exception{
         ResultActions action = mvc.perform(MockMvcRequestBuilders
-                .delete(url))
+                .delete(url+"/1"))
                 .andExpect(status().isOk());
 
     }
