a971ad90cec49c7c9756fb826eaa7b180773bedd
Eojin K1m <eojin.dev@gmail.com>
2020-08-09 21:53:00 +0900

    [MODIFY] <main/../entities/equipmentApply/EquipmentApply.java>
    modify ID column to auto increase

--- src/main/java/com/gym/dsm/fitness/entities/equipmentApply/EquipmentApply.java
+++ src/main/java/com/gym/dsm/fitness/entities/equipmentApply/EquipmentApply.java
@@ -16,7 +16,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class EquipmentApply {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne
