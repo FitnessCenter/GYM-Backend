052f39fa32a22545837c272769a42c8e54986ee7
Maly <migsking@naver.com>
2020-08-10 23:30:03 +0900

    [CREATE] <main/../security/JWTProvider.java>
    generate jwt

--- src/main/java/com/gym/dsm/fitness/security/JWTProvider.java
+++ src/main/java/com/gym/dsm/fitness/security/JWTProvider.java
@@ -1,12 +1,40 @@
 package com.gym.dsm.fitness.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
 public class JWTProvider {
-    public String generateAccessToken() {
-        return "eya.b.c";
+
+    @Value("${auth.jwt.secret}")
+    private String secretKey;
+
+    @Value("${auth.jwt.exp.access}")
+    private Long accessTokenExpireTime;
+
+    @Value("${auth.jwt.exp.refresh}")
+    private Long refreshTokenExpireTime;
+
+    @Value("${auth.jwt.header}")
+    private String header;
+
+    @Value("${auth.jwt.prefix}")
+    private String prefix;
+
+    private final AuthDetailsService authDetailsService;
+
+    public String generateAccessToken(String id) {
+        return Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpireTime * 1000))
+                .setSubject(id)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
     }
 }
