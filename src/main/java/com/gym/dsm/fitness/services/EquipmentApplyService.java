76febb3aec790b64e129c3fd125df7c77dd39df4
Eojin K1m <eojin.dev@gmail.com>
2020-08-11 09:01:00 +0900

    [ADD] <main/../services/EquipmentAppService*.*> Implement getEquipmentApplis

--- src/main/java/com/gym/dsm/fitness/services/EquipmentApplyService.java
+++ src/main/java/com/gym/dsm/fitness/services/EquipmentApplyService.java
@@ -1,12 +1,12 @@
 package com.gym.dsm.fitness.services;
 
+import com.gym.dsm.fitness.entities.equipmentApply.EquipmentApply;
 import com.gym.dsm.fitness.payloads.requests.EquipmentApplyRequest;
-import com.gym.dsm.fitness.payloads.responses.EquipmentApplyResponse;
 
 import java.util.List;
 
 public interface EquipmentApplyService {
-    List<EquipmentApplyResponse> getEquipmentApplies();
+    List<EquipmentApply> getEquipmentApplies();
     void createEquipmentApply(EquipmentApplyRequest request);
     void updateEquipmentApply(Integer id);
     void deleteEquipmentApply(Integer id);
