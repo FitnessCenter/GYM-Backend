ffdb16fb2bf41f023fc194e87c4692ac41f56645
Eojin K1m <eojin.dev@gmail.com>
2020-08-11 23:29:09 +0900

    [DELETE] <main/../FitnessApplication.java> remove modelMapper from here

--- src/main/java/com/gym/dsm/fitness/FitnessApplication.java
+++ src/main/java/com/gym/dsm/fitness/FitnessApplication.java
@@ -1,25 +1,11 @@
 package com.gym.dsm.fitness;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.config.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FitnessApplication {
-
-	@Bean
-	public ModelMapper modelMapper(){
-		ModelMapper modelMapper = new ModelMapper();
-		modelMapper.getConfiguration()
-				.setFieldMatchingEnabled(true)
-				.setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
-		return modelMapper;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(FitnessApplication.class, args);
 	}
-
 }
