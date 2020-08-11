933c3f556e22df3395e8de5aab40eca50fe76e88
Maly <migsking@naver.com>
2020-08-11 14:41:36 +0900

    [CREATE] <main/../auth/AuthenticationFacade.java>

---
+++ src/main/java/com/gym/dsm/fitness/security/auth/AuthenticationFacade.java
@@ -0,0 +1,10 @@
+package com.gym.dsm.fitness.security.auth;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class AuthenticationFacade {
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+}
