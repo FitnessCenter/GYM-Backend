9ef356166e4a0583080c945f3e6b4582c2e118c9
Eojin K1m <eojin.dev@gmail.com>
2020-08-11 19:57:45 +0900

    [DELETE] <main/../payloads/payloads/requests/EquipmentApplyRequest.java>> delete lombok setter annotation

--- src/main/java/com/gym/dsm/fitness/payloads/requests/EquipmentApplyRequest.java
+++ src/main/java/com/gym/dsm/fitness/payloads/requests/EquipmentApplyRequest.java
@@ -3,10 +3,8 @@ package com.gym.dsm.fitness.payloads.requests;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Builder
 public class EquipmentApplyRequest {
     private Integer numberOfApply;
