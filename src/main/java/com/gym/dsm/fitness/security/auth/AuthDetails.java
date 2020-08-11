d3465bd2024694dd645c2b78f3c7b9594ff83769
Maly <migsking@naver.com>
2020-08-11 19:15:55 +0900

    [CREATE] <main/../security/auth/AuthDetailsService.java>

---
+++ src/main/java/com/gym/dsm/fitness/security/auth/AuthDetails.java
@@ -0,0 +1,55 @@
+package com.gym.dsm.fitness.security.auth;
+
+import com.gym.dsm.fitness.entities.user.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@AllArgsConstructor
+public class AuthDetails implements UserDetails {
+
+    private User user;
+    private AuthorityType authorityType;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public AuthorityType getAuthorityType() {
+        return authorityType;
+    }
+}
