66a9fe2f222c7c44d79ff3d280a3cd0bad7ffe6c
Eojin K1m <eojin.dev@gmail.com>
2020-08-11 09:03:49 +0900

    [CREATE] <test/../Services/TestEquipmentApplyService.java> add when_get_equipment_apply_it_should_return_equipment_apply_list()

---
+++ src/test/java/com/gym/dsm/fitness/Services/TestEquipmentApplyService.java
@@ -0,0 +1,53 @@
+package com.gym.dsm.fitness.Services;
+
+import com.gym.dsm.fitness.entities.equipmentApply.EquipmentApply;
+import com.gym.dsm.fitness.entities.equipmentApply.repository.EquipmentApplyRepository;
+import com.gym.dsm.fitness.entities.user.User;
+import com.gym.dsm.fitness.payloads.requests.EquipmentApplyRequest;
+import com.gym.dsm.fitness.entities.equipmentApply.EquipmentApply;
+import com.gym.dsm.fitness.payloads.responses.EquipmentApplyResponse;
+import com.gym.dsm.fitness.services.EquipmentApplyService;
+import com.gym.dsm.fitness.services.EquipmentApplyServiceImp;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@ExtendWith(SpringExtension.class)
+public class TestEquipmentApplyService {
+
+    @Mock
+    EquipmentApplyRepository equipmentApplyRepository;
+
+    @InjectMocks
+    EquipmentApplyServiceImp equipmentApplyService;
+
+    private final EquipmentApplyRequest request = EquipmentApplyRequest.builder()
+            .build();
+
+    private final EquipmentApply equipmentApply = EquipmentApply.builder()
+            .id(1)
+            .build();
+
+
+
+    @Test
+    public void when_get_equipment_apply_it_should_return_equipment_apply_list(){
+        ArrayList<EquipmentApply> equipmentApplies = new ArrayList<>();
+        equipmentApplies.add(this.equipmentApply);
+        when(equipmentApplyRepository.findAll()).thenReturn(equipmentApplies);
+
+        List<EquipmentApply> gotEquipmentApplies = equipmentApplyService.getEquipmentApplies();
+        assertThat(gotEquipmentApplies.get(0).getId(), is(equipmentApply.getId()));
+    }
+}
