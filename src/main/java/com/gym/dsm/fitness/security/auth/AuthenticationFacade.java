6aa142c9909d8168d80dafa368ddfc502e48c397
Maly <migsking@naver.com>
2020-08-13 17:20:45 +0900

    [ADD] <main/../auth/AuthenticationFacade.java>
    @Component 추가

--- src/main/java/com/gym/dsm/fitness/security/auth/AuthenticationFacade.java
+++ src/main/java/com/gym/dsm/fitness/security/auth/AuthenticationFacade.java
@@ -2,7 +2,9 @@ package com.gym.dsm.fitness.security.auth;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthenticationFacade {
     public Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
