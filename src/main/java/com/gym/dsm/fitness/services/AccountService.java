fb1694835ad2d9ada1ffbe187de3ed0e598a3f20
Maly <migsking@naver.com>
2020-08-14 08:59:09 +0900

    [MODIFY] <main/../services/AccountService.java>

--- src/main/java/com/gym/dsm/fitness/services/AccountService.java
+++ src/main/java/com/gym/dsm/fitness/services/AccountService.java
@@ -1,10 +1,11 @@
 package com.gym.dsm.fitness.services;
 
 import com.gym.dsm.fitness.payloads.requests.CreateAccountRequest;
+import com.gym.dsm.fitness.payloads.requests.UpdatePasswordRequest;
 import com.gym.dsm.fitness.payloads.responses.GetAccountResponse;
 
 public interface AccountService {
     public GetAccountResponse getAccount();
     public void createAccount(CreateAccountRequest createAccountRequest);
-    public void updatePassword();
+    public void updatePassword(UpdatePasswordRequest updatePasswordRequest);
 }
