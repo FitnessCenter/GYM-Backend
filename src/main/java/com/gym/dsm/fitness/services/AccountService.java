f92e1e84c8d3ca5346fe642bba539884ac66e8f6
Maly <migsking@naver.com>
2020-08-14 08:48:15 +0900

    [MODIFY] <main/../services/AccountService.java>
    void로 변환

--- src/main/java/com/gym/dsm/fitness/services/AccountService.java
+++ src/main/java/com/gym/dsm/fitness/services/AccountService.java
@@ -1,11 +1,10 @@
 package com.gym.dsm.fitness.services;
 
 import com.gym.dsm.fitness.payloads.requests.CreateAccountRequest;
-import com.gym.dsm.fitness.payloads.responses.CreateAccountResponse;
 import com.gym.dsm.fitness.payloads.responses.GetAccountResponse;
 
 public interface AccountService {
     public GetAccountResponse getAccount();
-    public CreateAccountResponse createAccount(CreateAccountRequest createAccountRequest);
+    public void createAccount(CreateAccountRequest createAccountRequest);
     public void updatePassword();
 }
