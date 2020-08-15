ec43be24da018d18bdd3642f63201791de0f9a7e
asdf <migsking@naver.com>
2020-08-15 18:41:49 +0900

    [MODIFY] <test/controllers/TestAccountController.java>

--- src/test/java/com/gym/dsm/fitness/controllers/TestAccountController.java
+++ src/test/java/com/gym/dsm/fitness/controllers/TestAccountController.java
@@ -2,6 +2,7 @@ package com.gym.dsm.fitness.controllers;
 
 import com.gym.dsm.fitness.FitnessApplication;
 import com.gym.dsm.fitness.payloads.requests.CreateAccountRequest;
+import com.gym.dsm.fitness.payloads.requests.UpdatePasswordRequest;
 import com.gym.dsm.fitness.payloads.responses.GetAccountResponse;
 import com.gym.dsm.fitness.security.JWTFilter;
 import com.gym.dsm.fitness.security.JWTProvider;
@@ -111,19 +112,19 @@ class TestAccountController {
 
                 return requestAccount(postMockRequestBuilder.getRequest());
 
-//            case "PUT":
-//                UpdateAccountRequest updateAccountRequest = UpdateAccountRequest.builder()
-//                        .currentPassword("currentP@ssword")
-//                        .newPassword("p@ssword")
-//                        .build();
-//
-//                MockRequestBuilder putMockRequestBuilder = MockRequestBuilder.builder()
-//                        .requestBuilder(put(url))
-//                        .token(jwtProvider.generateAccessToken())
-//                        .body(updateAccountRequest)
-//                        .build();
-//
-//                return requestAccount(putMockRequestBuilder.getRequest());
+            case "PUT":
+                UpdatePasswordRequest updatePasswordRequest = UpdatePasswordRequest.builder()
+                        .currentPassword("currentP@ssword")
+                        .newPassword("p@ssword")
+                        .build();
+
+                MockRequestBuilder putMockRequestBuilder = MockRequestBuilder.builder()
+                        .requestBuilder(put(url))
+                        .token("Bearer eya.b.c")
+                        .body(updatePasswordRequest)
+                        .build();
+
+                return requestAccount(putMockRequestBuilder.getRequest());
 
             default:
                 throw new Exception();
