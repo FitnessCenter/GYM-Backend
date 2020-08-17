d407168f21b7d5f1cc6888b43c42700107dec959
asdf <migsking@naver.com>
2020-08-17 19:22:26 +0900

    [ADD] <main/../controllers/AuthController.java>
    아규 먼트 추가

--- src/main/java/com/gym/dsm/fitness/controllers/AuthController.java
+++ src/main/java/com/gym/dsm/fitness/controllers/AuthController.java
@@ -1,11 +1,13 @@
 package com.gym.dsm.fitness.controllers;
 
+import com.gym.dsm.fitness.payloads.requests.SignInRequest;
+import com.gym.dsm.fitness.payloads.responses.SignInResponse;
+import com.gym.dsm.fitness.services.Auth.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
@@ -15,7 +17,7 @@ public class AuthController {
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.OK)
-    public SignInResponse signIn() {
-        return authService.signIn();
+    public SignInResponse signIn(@RequestBody @Valid SignInRequest signInRequest) {
+        return authService.signIn(signInRequest);
     }
 }
